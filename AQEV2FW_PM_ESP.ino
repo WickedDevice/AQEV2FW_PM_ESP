a57ccf4c8a735a07e99c642fdeee324239054a3b
Victor Aprea <victor.aprea@wickeddevice.com>
2017-05-09 16:33:05 -0400

    backlight behavior changes, and longer connect timeout in softAP network check

--- AQEV2FW_PM_ESP.ino
+++ AQEV2FW_PM_ESP.ino
@@ -507,7 +507,6 @@ void setup() {
 
   // initialize hardware
   initializeHardware();
-  backlightOff();
 
   //  uint8_t tmp[EEPROM_CONFIG_MEMORY_SIZE] = {0};
   //  get_eeprom_config(tmp);
@@ -631,6 +630,7 @@ void setup() {
       }
     }
     Serial.println();
+    backlightOn();
 
     if(soft_ap_config_activated){
       configInject("aqe\r");
@@ -721,11 +721,11 @@ void setup() {
       prompt();
       for (;;) {
         current_millis = millis();
-        if(current_millis - previous_touch_sampling_millis >= touch_sampling_interval){
-          previous_touch_sampling_millis = current_millis;
-          collectTouch();
-          processTouchQuietly();
-        }
+//        if(current_millis - previous_touch_sampling_millis >= touch_sampling_interval){
+//          previous_touch_sampling_millis = current_millis;
+//          collectTouch();
+//          processTouchQuietly();
+//        }
 
         // check to determine if we have a GPS
         while(!gps_installed && gpsSerial.available()){
@@ -888,6 +888,7 @@ void setup() {
   }
 
   resumeGpsProcessing();
+  backlightOff();
 }
 
 void loop() {
@@ -6718,7 +6719,7 @@ boolean parseConfigurationMessageBody(char * body){
       set_network_password(pwd);
       set_network_security_mode("auto");
 
-      if(esp.connectToNetwork((char *) ssid, (char *) pwd, 30000)){
+      if(esp.connectToNetwork((char *) ssid, (char *) pwd, 45000)){
         Serial.print(F("Info: Successfully connected to Network \""));
         Serial.print(ssid);
         Serial.print(F("\""));
