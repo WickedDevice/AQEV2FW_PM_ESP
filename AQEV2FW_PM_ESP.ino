cd2699f9e8bab55f6c23fd1a2df4431c2365e9e9
Victor Aprea <victor.aprea@wickeddevice.com>
2017-10-26 15:04:50 -0400

    fw update on lcd screen should not say attempting force restart

--- AQEV2FW_PM_ESP.ino
+++ AQEV2FW_PM_ESP.ino
@@ -5415,11 +5415,17 @@ void delayForWatchdog(void){
 }
 
 void watchdogForceReset(void){
+  watchdogForceReset(true);
+}
+
+void watchdogForceReset(boolean changeLCD){
   Serial.println(F("Info: Attempting Watchdog Forced Restart."));
-  setLCD_P(PSTR("   ATTEMPTING   "
-                "  FORCED RESET  "));
-  backlightOn();
-  ERROR_MESSAGE_DELAY();
+  if(changeLCD){
+    setLCD_P(PSTR("   ATTEMPTING   "
+                  "  FORCED RESET  "));
+    backlightOn();
+    ERROR_MESSAGE_DELAY();
+  }
   
   tinywdt.force_reset();
   Serial.println(F("Error: Watchdog Force Restart failed. Manual reset is required."));
@@ -6072,7 +6078,7 @@ void checkForFirmwareUpdates(){
                         "WAIT ONE MINUTE "));
           lcdSmiley(15, 1);
           SUCCESS_MESSAGE_DELAY();
-          watchdogForceReset();
+          watchdogForceReset(false);
         }
         else{
           Serial.println(F("Error: Firmware Update Failed. Try again later by resetting."));
